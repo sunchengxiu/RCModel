03d89fcdf245c9265c27c10b4d975da91fa73b0d
孙承秀 <>
2018-10-24 14:04:55 +0800

    增加注释

--- RCModel/RCModel/RCModel/NSObject+RCModel.h
+++ RCModel/RCModel/RCModel/NSObject+RCModel.h
@@ -20,14 +20,38 @@ NS_ASSUME_NONNULL_BEGIN
 @interface NSObject (RCModel)
 
 /**
- 将 json 转化为 model（可识别 NSData ， NSString ， NSDictionary）
+ 将 json 转化为 model（可识别 NSData ， NSString ， NSDictionary），会先将传进来的数据格式化成 dic ， 然后调用下面的方法。
  
  @param json json 数据
  @return 转化后的 model
  */
 + (instancetype)modelWithJson:(id)json;
-- (BOOL)modelSetWithDictionary:(NSDictionary *)dic;
+
+/**
+ dic -> model，模型序列化，最终会调用下面的方法进行k核心模型转换
+
+ @param dic 需要转换的 dic
+ @return 转换后的模型
+ */
 + (instancetype)modelWithDictionary:(NSDictionary *)dic;
+
+/**
+ 根据传进来的 dic，进行 dic -> model 的操作，返回值代表是否转换成功，可以通过一个模型，直接调用这个方法，穿进去对应的 dic ， 进行模型转化
+ example:
+    @interface RCUser : NSObject
+
+     @property (nonatomic, strong) RCAddress *address;
+
+     @end
+ 
+    [_address modelSetWithDictionary:dic[@"address"]];
+ 
+    可以使用上面的方式直接转化
+ @param dic 需要转换的 dic
+ @return 是否转换成功
+ */
+- (BOOL)modelSetWithDictionary:(NSDictionary *)dic;
+
 @end
 
 NS_ASSUME_NONNULL_END
